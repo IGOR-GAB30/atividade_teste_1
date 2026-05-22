8dd463def8e4b6ba591319305a2f159d9f4d86da
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-05-22 10:06:33 -0300

    Add- Primeiro código com o uso de php e inicialização de banco de dados

---
+++ infra/redes/script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE sistema_simples;
+
+USE sistema_simples;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY;
+    usuario VARCHAR(255) NOT NULL;
+    senha VARCHAR(255) NOT NULL;
+);
+
+INSERT INTO usuario (usuario, senha) VALUES ('admin','1234');
+
+ 
